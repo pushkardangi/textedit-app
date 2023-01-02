2716f26fb53d37ce90eda324368b9a4fa1745d47
Pushkar <pushkardangi746@gmail.com>
2023-01-02 17:36:41 +0530

    05 completed

--- src/App.js
+++ src/App.js
@@ -5,17 +5,27 @@ function App() {
   return (
     <>
       <nav>
-        <ul className="nav">
-          <li className="nav-item">Home</li>
-          <li className="nav-item">About</li>
-          <li className="nav-item">Contact</li>
-        </ul>
+        <div className="navbar">
+          <div className="nav">
+            <ul className="nav-part-one">
+              <li className="nav-item app-name"><a href="/">Textedit</a></li>
+              <li className="nav-item nav-link"><a href="/">Home</a></li>
+              <li className="nav-item nav-link"><a href="/">About</a></li>
+              <li className="nav-item nav-link"><a href="/">Contact</a></li>
+            </ul>
+          </div>
+
+          <div className="nav-part-two">
+            <div><input className="inputs" type="text" name="search" id="input1" /></div>
+            <div><input className="inputs" type="button" value="Search" id="input2" /></div>
+          </div>
+
+        </div>
       </nav>
 
       <main className="main">
-        <h1>Hello World</h1>
         <div className="container">
-
+          container
         </div>
       </main>
     </>
